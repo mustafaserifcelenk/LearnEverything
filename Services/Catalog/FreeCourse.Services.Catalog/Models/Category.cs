6e084d850b3d2f4638953a936453351111543cf0
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2021-10-18 17:18:24 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]  // Id'yi string olarak alıp ObjectId'ye dönüştürüyor, getirirken de string'e dönüştürüp veriyor
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Shared/FreeCourse.Shared/Dtos/NoContent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class NoContent
+    {
+        // Data dönülmeyecek response'lar için boş obje olarak kullanılacak
+    }
+}
