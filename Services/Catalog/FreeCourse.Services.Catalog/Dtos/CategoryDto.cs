4daa73572ae96a6c635ab3816fccce75ab23d524
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2021-10-18 18:02:14 +0300

    Section 2: Catalog.API(Microservice) : Create Dtos

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FreeCourse.Services.Catalog.Dtos
+{
+    public class CategoryDto
+    {
+        // Course için yapılan create dto'ları kategori için de yapılabilir
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
