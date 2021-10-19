35e5558b824280966369aef031477c5c06d54826
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2021-10-19 14:31:56 +0300

    Section 2: Catalog.API(Microservice) : Database Configuration & OptionsPattern

--- Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
@@ -1,3 +1,4 @@
+using FreeCourse.Services.Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -25,8 +27,22 @@ namespace FreeCourse.Services.Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAutoMapper()
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
+
+            #region OptionsPattern
+            //AppSettings.json'ý okuyup class'a aktarma: Options Pattern.
+            services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
+            services.AddSingleton<IDatabaseSettings>(sp =>
+            {
+                // Service Provider(sp) veriyor, bu fonksiyon belirttiðimiz interface'i implement eden class dönüyor
+                // GetRequired Service'in Get Service'ten farký dönüþ olmazsa hata fýrlatýr
+                return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+            });
+
+            // Böylece IDatabaseSettings geçtiðimizde GetSection ile okunan deðerler sýnýf olarak elimize geçecek
+            #endregion
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.Catalog", Version = "v1" });
