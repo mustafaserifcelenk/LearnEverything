9cd6bb90ec2f408515ea0d7625790e2802053725
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2021-10-19 17:15:04 +0300

    Section 2: Catalog.API(Microservice) : CoursesController

---
+++ Shared/FreeCourse.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,20 @@
+﻿using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.ControllerBases
+{
+    public class CustomBaseController : ControllerBase
+    {
+        // Bu controller sayesinde dönüş tipleri action olarak tutarken status kodları da duruma göre atıyoruz
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
