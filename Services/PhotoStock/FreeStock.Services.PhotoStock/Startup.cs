83b913db690f1a490842e5b8c054e7bc3bba2dd2
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2021-10-25 11:19:51 +0300

    Section 4: PhotoStock.API(Microservice) Settings

--- Services/PhotoStock/FreeStock.Services.PhotoStock/Startup.cs
+++ Services/PhotoStock/FreeStock.Services.PhotoStock/Startup.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,8 +27,16 @@ namespace FreeStock.Services.PhotoStock
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "photo_stock_catalog";
+                options.RequireHttpsMetadata = false;
+            });
+            services.AddControllers(opt =>
+            {
+                opt.Filters.Add(new AuthorizeFilter());
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeStock.Services.PhotoStock", Version = "v1" });
@@ -46,6 +56,8 @@ namespace FreeStock.Services.PhotoStock
             app.UseStaticFiles(); // wwwroot klasörüne eriþim için bunu eklememiz gerekiyor     
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
