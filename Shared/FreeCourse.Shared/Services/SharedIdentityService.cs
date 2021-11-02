0af31328f350d47ac2e428cbd13513205c6fd31b
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2021-11-02 09:10:25 +0300

    Section 5 : Basket.API(Microservice) SharedIdentityService

---
+++ Shared/FreeCourse.Shared/Services/SharedIdentityService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.Services
+{
+    public class SharedIdentityService : ISharedIdentityService
+    {
+        // Kullanıcılar HttpContext üzerinden user propertilerinde claimler olarak geliyor
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public SharedIdentityService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetUserId => _httpContextAccessor.HttpContext.User.FindFirst("sub").Value;
+    }
+}
