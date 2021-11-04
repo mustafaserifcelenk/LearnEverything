c711a21d87f1088858cc8a226a506cc0fdbaed3e
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2021-11-04 12:47:02 +0300

    Section 6 : Discount.API(Microservice) Discount Model

---
+++ Services/Discount/FreeCourse.Services.Discount/Models/Discount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FreeCourse.Services.Discount.Models
+{
+    // Postgre içindeki discount tablosuna modeli mapledik
+    [Dapper.Contrib.Extensions.Table("discount ")]
+    public class Discount
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int Rate { get; set; }
+        public string Code { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
