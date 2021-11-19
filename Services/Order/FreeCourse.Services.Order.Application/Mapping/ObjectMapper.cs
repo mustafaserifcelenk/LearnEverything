d4a40d64a51696203b7143c977fdc5737fb43fc3
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2021-11-19 08:18:38 +0300

    Section 7 : Order:API(Microservice) Dtos & Mapping

---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Mapping
+{
+    public class ObjectMapper
+    {
+        // Lazy sınıfı mappinge istek yapılana kadar map sınıflarını oluşturmaz
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomMapping>();
+            });
+            return config.CreateMapper();
+        });
+        
+        // bu Mapper sınıfı çağrıldığı an lazy sınıfının değerini dön demek
+        public static IMapper Mapper => lazy.Value;
+
+}
+}
