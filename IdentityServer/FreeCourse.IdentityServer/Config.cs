cf2e4b9edd0a9af200cefd0f751109469e05c99d
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2021-11-24 12:54:39 +0300

    Section 9 : Gateway(Microservice) Configurations

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -20,6 +20,7 @@ namespace FreeCourse.IdentityServer
             new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
             new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
             new ApiResource("resource_payment"){Scopes={"payment_fullpermission"}},
+            new ApiResource("resource_gateway"){Scopes={"gateway_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -44,6 +45,7 @@ namespace FreeCourse.IdentityServer
                 new ApiScope("discount_fullpermission", "Discount API için full erişim"),
                 new ApiScope("order_fullpermission", "Order API için full erişim"),
                 new ApiScope("payment_fullpermission", "Payment API için full erişim"),
+                new ApiScope("gateway_fullpermission", "Gateway API için full erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -56,7 +58,7 @@ namespace FreeCourse.IdentityServer
                     ClientName = "Asp.Net Core MVC",
                     ClientSecrets = {new Secret("secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    AllowedScopes={"catalog_fullpermission", "photo_stock_fullpermission", IdentityServerConstants.LocalApi.ScopeName}
+                    AllowedScopes={"catalog_fullpermission", "photo_stock_fullpermission", "gateway_fullpermission",IdentityServerConstants.LocalApi.ScopeName}
                 },
                 new Client
                 {
@@ -65,7 +67,7 @@ namespace FreeCourse.IdentityServer
                     AllowOfflineAccess=true,
                     ClientSecrets = {new Secret("secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes={"discount_fullpermission", "basket_fullpermission","order_fullpermission", "payment_fullpermission", IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess,IdentityServerConstants.LocalApi.ScopeName,"roles"},
+                    AllowedScopes={"discount_fullpermission", "basket_fullpermission","order_fullpermission", "payment_fullpermission", "gateway_fullpermission", IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess,IdentityServerConstants.LocalApi.ScopeName,"roles"},
                     AccessTokenLifetime=1*60*60,
                     RefreshTokenExpiration=TokenExpiration.Absolute,
                     // Refresh Token eğer kullanıcı offline'sa token'ın 60 güne kadar kullanılmasını sağlayan özellik
