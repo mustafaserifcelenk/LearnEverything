24b561bfa47c91cef6f85f18b283a5614333e7bb
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2021-12-06 13:50:38 +0300

    Section 10 : Asp.Net Core MVC App(Microservice) UserController

---
+++ Frontends/FreeCourse.Web/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Getuser çağrıldığında delege araya giriyor(startupda eklenen kod sayesinde) token'ı alıyor
+            return View(await _userService.GetUser());
+        }
+    }
+}
