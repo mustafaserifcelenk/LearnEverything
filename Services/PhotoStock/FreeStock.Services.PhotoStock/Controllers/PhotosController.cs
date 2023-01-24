53439f7d787880325b29046b1dbfdd0ff23dfdc5
Mustafa Şerif Çelenk <mustafaserifcelenk@gmail.com>
2023-01-24 20:19:27 +0300

    Dockerize

--- Services/PhotoStock/FreeStock.Services.PhotoStock/Controllers/PhotosController.cs
+++ Services/PhotoStock/FreeStock.Services.PhotoStock/Controllers/PhotosController.cs
@@ -36,6 +36,7 @@ namespace FreeStock.Services.PhotoStock.Controllers
             return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty.",400));
         }
 
+        [HttpDelete]
         public IActionResult PhotoDelete(string photoUrl)
         {
             var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
